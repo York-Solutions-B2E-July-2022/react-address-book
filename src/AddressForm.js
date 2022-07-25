953e7aa1b278185ebca53414a2f3aacf5688eb37
adam-street <adam.street.dev@gmail.com>
2022-07-25 17:07:36 -0500

    init

---
+++ src/AddressForm.js
@@ -0,0 +1,49 @@
+import {useState} from "react";
+
+export default function AddressForm(props) {
+
+    // useState is a function provided by React to allow us to persist data through re-rendering
+    // this function returns an array with 2 items [the value, a function to update the value]
+    // NEVER change the value directly ONLY ONLY ONLY change the value via the function provided (the set function)
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+
+    // this is the function that I want to run when the form submit button is clicked by the user
+    // this is my programs "reaction" to the user submitting the form
+    function onAddressFormSubmit(event) {
+        // the default action of a form submit is to refresh the page
+        // this will prevent the default action (this will stop the page from refreshing)
+        event.preventDefault();
+
+        // when the form is submitted we want to run the function on the app component to add the address to the screen
+        // the function we want to run is stored in the props object under the name "addAddress"
+        // so when I call this function here I am actually running a function on my App component
+        props.addAddress(firstName, lastName, email)
+    }
+
+    return <form onSubmit={onAddressFormSubmit}>
+        <label>
+            Fist Name
+            {/* We are setting the value of the input to whatever value is stored in the state for "firstName"*/}
+            <input type={'text'} value={firstName} onChange={e => setFirstName(e.target.value)}/>
+        </label>
+        <br/>
+        <label>
+            Last Name
+            {/* the "onChange" prop here is setting up a function that will call the set function for the state variable lastName*/}
+            {/* We are updating the state with the new value for the input box */}
+            {/* We are saving the user input in the lastName state */}
+            <input type={'text'} value={lastName} onChange={e => setLastName(e.target.value)}/>
+        </label>
+        <br/>
+        <label>
+            Email
+            <input type={'email'} value={email} onChange={e => setEmail(e.target.value)}/>
+        </label>
+
+        {/* If a button is inside a form element its default is to submit the form */}
+        {/* this is why we do not need to add an "onClick" prop */}
+        <button>Submit</button>
+    </form>
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import AddressForm from "./AddressForm";
+import {useState} from "react";
 
+// app is the "root" component is it the component that is rendered first
+// all other components are "children" of the app component
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // this is "array destructuring" all that means is that the array that is reutrned from the useState() function
+    // is "indexed" and stored into separate variables for us!
+    // the array destructing is the same as saying
+    /*
+            const addressState = useState([]);
+            const addressList = addressState[0];
+            const setAddressList = addressState[1];
+     */
+    const [addressList, setAddressList] = useState([]);
+
+    function addAddressToList(fistName, lastName, email) {
+        setAddressList(
+            [
+                ...addressList,
+                {fistName: fistName, lastName: lastName, email: email}
+            ]
+        )
+    }
+
+    return <div>
+        {/*
+            We are rendering the "AddressForm" component here and passing a "property" under the name "addAddress"
+            Properties are just a way to pass data to a child component
+                In this case we are using this property to pass a function (addAddressToList()) to the child it can call
+                that function and have the ability to cause a re-render on its parent component. (in this case the App comp)
+        */}
+        <AddressForm addAddress={addAddressToList}></AddressForm>
+
+        {/*
+            We need to render an array of objects, so we can see each piece of address data the user has input
+            React can render an array without issue (as long as we provide a unique key property to each item in the array),
+            but React cannot render an object. So we are using the js map function to convert the array of objects into an array
+            of JSX
+        */}
+        {addressList.map(
+            (obj, idx) => <div key={idx}>
+                Name: {obj.firstName} {obj.lastName}
+                <br/>
+                Email: {obj.email}
+            </div>
+        )}
     </div>
-  );
 }
 
 export default App;
